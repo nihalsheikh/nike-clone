deb93f5afafa6658216cf5aeb8e1314403362b26
nihal <nihalsheikh585@gmail.com>
2025-09-30 10:04:17 +0530

    ADDED: Multiple sections created

---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import PopularProducts from "./PopularProducts";
+import CustomerReviews from "./CustomerReviews";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+import Subscribe from "./Subscribe";
+import Services from "./Services";
+import Footer from "./Footer";
+import Hero from "./Hero";
+
+
+export {
+	PopularProducts,
+	CustomerReviews,
+	SuperQuality,
+	SpecialOffer,
+	Subscribe,
+	Services,
+	Footer,
+	Hero,
+};
